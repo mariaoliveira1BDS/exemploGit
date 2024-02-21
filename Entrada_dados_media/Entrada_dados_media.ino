4f63145df7283140a0255084bde49de6fff98d8c
MariaClaraOliveira08 <mahh.oliveeira16@gmail.com>
2024-02-21 10:06:40 -0300

    initial commit

---
+++ Entrada_dados_media/Entrada_dados_media.ino
@@ -0,0 +1,16 @@
+//Declaração das variáveis / números reais*/
+float nota1, nota2, nota3, peso1, peso2, peso3, media;
+
+
+void setup() {
+  //Inicia a comunicação com o Serial Monitor
+  Serial.begin(9600);
+
+  delay(1000); //Espera estratégica
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
